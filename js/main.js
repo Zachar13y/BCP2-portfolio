856c4e96314e0e1c697a1736b4980b5d55f20690
Zachar13y <zachar13y@gmail.com>
2018-10-12 11:57:11 -0700

    Started on hamburger javascript

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
 "use strict";
 
+// function dropdown(event) {
+    var hamburger = document.querySelector("hamburger");
+    var fullMenu = document.querySelector('.nav-drop');
+
+    console.log(hamburger);
+
+    hamburger.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (window.getComputedStyle(hamburger).display === 'block') {
+            hamburger.style.display = 'none';
+        } else {
+            hamburger.style.display = 'block';
+        }
+    });
+
+//     const menu = document.querySelector('.menu');
+//     const menuToggle = document.querySelector('menuToggle');
+
+//   menuToggle.addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//     if (window.getComputedStyle(menu).display === 'block') {
+//       menu.style.display = 'none';
+//     } else  {
+//       menu.style.display = 'block';
+//     }
+//   });
+    
+// }
