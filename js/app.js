61917ffdc7df922685cf056b468b56f5b31978aa
Zachar13y <zachar13y@gmail.com>
2018-10-14 17:45:29 -0700

    Made data for projects, built constructor function for data.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var projects = [];
+
+function Project(rawObjData) {
+    this.title = title;
+    this.icon = icon;
+    this.description = description;
+    this.screenshots = screenshots;
+}
+
---
+++ js/projectData.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var rawData = [{
+    title: 'jam-time',
+    description: '<p>Jam Time is a delivery service app that was developed for  & a sandwich company</p>',
+    screenshots: '<img src="profile-jam.jpg">',
+},
+{
+    title: 'bus-mall',
+    description: '<p>Bus Mall is a marketing app that was developed for a transportation company</p>',
+    screenshots: '<img src="profile-portland.jpg">',
+},
+{
+    title: 'salmon-cookies',
+    description: '<p>Salmon Cookies is a market analysis app that was developed for a cookie company</p>',
+    screenshots: '<img src="profile-salmon.jpg">',
+},
+{
+    title: 'sipster',
+    description: '<p>Sipster is a coffee shop locator app that was developed for a dude who loves coffee</p>',
+    screenshots: '<img src="profile-coffee.jpg">',
+},
+];
