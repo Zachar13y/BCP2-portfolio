2f20948a4a6e3bc44400ce83bb50670cd932790c
Zachar13y <zachar13y@gmail.com>
2018-10-15 17:31:10 -0700

    Projects layout done.

--- js/main.js
+++ js/main.js
@@ -4,20 +4,16 @@ var hamburger = document.querySelector('.hamburger');
 var fullMenu = document.querySelector('.nav-drop');
 
 function dropDown() {
-        // removeMenu = function(){
-        //     fullMenu.style.display === 'none';
-        // }
-        if (hamburger.style.display === 'block') {
+    if (hamburger.style.display === 'block') {
             hamburger.style.display = 'none';
             fullMenu.style.display = 'block';
     } 
     else {
         hamburger.style.display = 'block';
-        fullMenu.style.display = 'none';
+        // fullMenu.style.display = 'none';
     }
 };
 
-// $('nav-drop').toggleClass('display')
 window.addEventListener('click', function(){
     fullMenu.style.display === 'none';
 });
