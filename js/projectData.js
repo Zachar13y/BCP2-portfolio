dc64e645dbc16f8ed914312155ec87380d23ea40
Zachar13y <zachar13y@gmail.com>
2018-10-28 17:11:59 -0700

    Made project images link to project page, added a .gitignore file, styled icons.

--- js/projectData.js
+++ js/projectData.js
@@ -22,3 +22,20 @@ var rawObjData = [{
 }
 ];
 
+
+// Notes
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+// app.use(express.static('.public'));
+
+// app.get('/', function(request, response) {
+//     response.sendFile('./public/index.html');
+// });
+
+// app.listen(PORT, function() {
+//     console.log(`Listening on port ${PORT}...`);
+// });
+
